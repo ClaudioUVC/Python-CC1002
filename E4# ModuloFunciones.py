62cda55d7fb4d35993c982866eeefac4c73102c2
ClaudioUVC <clous.csd@gmail.com>
2014-08-11 15:28:04 -0400

    Update E4# ModuloFunciones.py

--- E4# ModuloFunciones.py
+++ E4# ModuloFunciones.py
@@ -1,15 +1,20 @@
-#perimetro
+#perimetrotriangulo: num num num -> num
+#Calcula el perimetro de un triangulo
+#ej: perimetrotriangulo(3,4,5) es 12
 def perimetrotriangulo(a,b,c):
     return a+b+c
 
-#area
-#num->num
+#areatriangulo: num num num -> num
+#Calcula el area de un triangulo
+#ej: areatriangulo(3,4,5) es 6
 def areatriangulo(a,b,c):
     import math
     s = ((a+b+c)/2.0)
     return math.sqrt(s*(s-a)*(s-b)*(s-c))
 
-#valido
+#triangulovalido: num num num -> boolean
+#Indica si los lados proporcionados son capaces de formar un triangulo
+#ej: triangulo(3,4,5) entregara "True"
 def triangulovalido(a,b,c):
     if a>0 and b>0 and c>0:
         if ((a+b)>c) and ((b+c)>a) and ((c+a)>b):
@@ -19,7 +24,9 @@ def triangulovalido(a,b,c):
     else:
         return False
 
-#tipo
+#tipotriangulo: num num num -> str
+#Identifica el tipo de triangulo a partir de los lados entregados.
+#ej: tipotriangulo(3,4,5) es Escaleno
 def tipotriangulo (a,b,c):
     if a==b and b==c:
         print "Equilatero."
@@ -29,7 +36,9 @@ def tipotriangulo (a,b,c):
         else:
             print "Escaleno."
 
-#rectangulo
+#triangulorectangulo: num num num -> boolean
+#Identifica si el triangulo formado es rectangulo o no.
+#ej: triangulorectangulo(3,4,5) entregara "True"
 def triangulorectangulo(a,b,c):
     if ((a*a)+(b*b))==(c*c):
         return True
